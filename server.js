97422cb8265acd4dda2289f52a7c00a42382f66c
JDVapor <jdvapordestiny@gmail.com>
2019-04-17 10:57:54 -0700

    test

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
-// var reservations = [
-//   {
-//     customerName: "yoda",
-//     customerEmail: "test@tessst.com",
-//     phoneNumber: "555-555-5555"
-//   },
-//
-//
-// ];
+var reservations = [{
+  customerName: "yoda",
+  customerEmail: "test@test.com",
+  phoneNumber: "555-555-5555",
+  userName: "forexe"
+}];
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
@@ -28,45 +27,20 @@ app.get("/tables", function(req, res) {
 app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
-//
-// app.get("/ch", function(req, res) {
-//   return res.json(characters);
-// });
-//
-// // Displays a single character, or returns false
-// app.get("/api/characters/:character", function(req, res) {
-//   var chosen = req.params.character;
-//
-//   console.log(chosen);
-//
-//   for (var i = 0; i < characters.length; i++) {
-//     if (chosen === characters[i].routeName) {
-//       return res.json(characters[i]);
-//     }
-//   }
-//
-//   return res.json(false);
-// });
-//
-// // Create New Characters - takes in JSON input
-// app.post("/api/characters", function(req, res) {
-//   // req.body hosts is equal to the JSON post sent from the user
-//   // This works because of our body parsing middleware
-//   var newcharacter = req.body;
-//
-//   // Using a RegEx Pattern to remove spaces from newCharacter
-//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//   newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-//
-//   console.log(newcharacter);
-//
-//   characters.push(newcharacter);
-//
-//   res.json(newcharacter);
-// });
 
-// Starts the server to begin listening
-// =============================================================
+app.post("/api/reserve", function(req, res) {
+
+  var newReservation = req.body;
+
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  reservations.push(newReservation);
+
+  res.json(newReservation);
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
